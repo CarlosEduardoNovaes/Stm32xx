8a2478e6e24db33379fbd58a399cbf5bcc12a746
Carlos Eduardo Novaes <carlosnov@gmail.com>
2018-12-16 23:33:27 -0200

    RCC and some GPIO done. First blink app running

--- include/stm32xx/registers/rcc_lmhxl.hpp
+++ include/stm32xx/registers/rcc_lmhxl.hpp
@@ -31,15 +31,15 @@ namespace rcc_cr{
     
     STM32_REGISTER_START(bitfield_t, 0x40021000)
         STM32_ACCESSOR_BB(Hsion     , 0)
-        STM32_ACCESSOR_BB(Hsirdy    , 1)
+        STM32_GETTER_BB  (Hsirdy    , 1)
         STM32_ACCESSOR_BF(Hsitrim   , uint32_t)
-        STM32_ACCESSOR_BF(Hsical    , uint32_t)
+        STM32_GETTER_BF  (Hsical    , uint32_t)
         STM32_ACCESSOR_BB(Hseon     , 16)
-        STM32_ACCESSOR_BB(Hserdy    , 17)
+        STM32_GETTER_BB  (Hserdy    , 17)
         STM32_ACCESSOR_BB(Hsebyp    , 18)
         STM32_ACCESSOR_BB(Csson     , 19)
         STM32_ACCESSOR_BB(Pllon     , 24)
-        STM32_ACCESSOR_BB(Pllrdy    , 25)
+        STM32_GETTER_BB  (Pllrdy    , 25)
     STM32_REGISTER_END
 };
 
@@ -163,27 +163,335 @@ namespace rcc_cir{
         uint32_t  _res_24           :   8;  // 24
     };
     
-    STM32_REGISTER_START(bitfield_t, 0x40021000)
-        STM32_ACCESSOR_BB(Lsirdyf   , 0)
-        STM32_ACCESSOR_BB(Lserdyf   , 1)
-        STM32_ACCESSOR_BB(Hsirdyf   , 2)
-        STM32_ACCESSOR_BB(Hserdyf   , 3)
-        STM32_ACCESSOR_BB(Pllrdyf   , 4)
-        STM32_ACCESSOR_BB(Cssf      , 7)
+    STM32_REGISTER_START(bitfield_t, 0x40021008)
+        STM32_GETTER_BB  (Lsirdyf   , 0)
+        STM32_GETTER_BB  (Lserdyf   , 1)
+        STM32_GETTER_BB  (Hsirdyf   , 2)
+        STM32_GETTER_BB  (Hserdyf   , 3)
+        STM32_GETTER_BB  (Pllrdyf   , 4)
+        STM32_GETTER_BB  (Cssf      , 7)
         STM32_ACCESSOR_BB(Lsirdye   , 9)
         STM32_ACCESSOR_BB(Lserdye   , 10)
         STM32_ACCESSOR_BB(Hsirdye   , 11)
         STM32_ACCESSOR_BB(Hserdye   , 12)
         STM32_ACCESSOR_BB(Pllrdye   , 13)
-        STM32_ACCESSOR_BB(Lsirdyc   , 16)
-        STM32_ACCESSOR_BB(Lserdyc   , 17)
-        STM32_ACCESSOR_BB(Hsirdyc   , 18)
-        STM32_ACCESSOR_BB(Hserdyc   , 19)
-        STM32_ACCESSOR_BB(Pllrdyc   , 20)
-        STM32_ACCESSOR_BB(Cssc      , 23)
+        STM32_SETTER_BB  (Lsirdyc   , 16)
+        STM32_SETTER_BB  (Lserdyc   , 17)
+        STM32_SETTER_BB  (Hsirdyc   , 18)
+        STM32_SETTER_BB  (Hserdyc   , 19)
+        STM32_SETTER_BB  (Pllrdyc   , 20)
+        STM32_SETTER_BB  (Cssc      , 23)
     STM32_REGISTER_END
 };
 
+// RCC_APB2RSTR Register
+namespace rcc_apb2rstr{
+    struct bitfield_t{
+        uint32_t  Afio              :   1;  // 00
+        uint32_t  _res_01           :   1;  // 01
+        uint32_t  Iopa              :   1;  // 02
+        uint32_t  Iopb              :   1;  // 03
+        uint32_t  Iopc              :   1;  // 04
+        uint32_t  Iopd              :   1;  // 05
+        uint32_t  Iope              :   1;  // 06
+        uint32_t  Iopf              :   1;  // 07
+        uint32_t  Iopg              :   1;  // 08
+        uint32_t  Adc1              :   1;  // 09
+        uint32_t  Adc2              :   1;  // 10
+        uint32_t  Tim1              :   1;  // 11
+        uint32_t  Spi1              :   1;  // 12
+        uint32_t  Tim8              :   1;  // 13
+        uint32_t  Usart1            :   1;  // 14
+        uint32_t  Adc3              :   1;  // 15
+        uint32_t  _res_16           :   3;  // 16
+        uint32_t  Tim9              :   1;  // 19
+        uint32_t  Tim10             :   1;  // 20
+        uint32_t  Tim11             :   1;  // 21
+        uint32_t  _res_24           :   10; // 22
+    };
+    
+    STM32_REGISTER_START(bitfield_t, 0x4002100C)
+        STM32_ACCESSOR_BB(Afio      , 0)
+        STM32_ACCESSOR_BB(Iopa      , 2)
+        STM32_ACCESSOR_BB(Iopb      , 3)
+        STM32_ACCESSOR_BB(Iopc      , 4)
+        STM32_ACCESSOR_BB(Iopd      , 5)
+        STM32_ACCESSOR_BB(Iope      , 6)
+        STM32_ACCESSOR_BB(Iopf      , 7)
+        STM32_ACCESSOR_BB(Iopg      , 8)
+        STM32_ACCESSOR_BB(Adc1      , 9)
+        STM32_ACCESSOR_BB(Adc2      , 10)
+        STM32_ACCESSOR_BB(Tim1      , 11)
+        STM32_ACCESSOR_BB(Spi1      , 12)
+        STM32_ACCESSOR_BB(Tim8      , 13)
+        STM32_ACCESSOR_BB(Usart1    , 14)
+        STM32_ACCESSOR_BB(Adc3      , 15)
+        STM32_ACCESSOR_BB(Tim9      , 19)
+        STM32_ACCESSOR_BB(Tim10     , 20)
+        STM32_ACCESSOR_BB(Tim11     , 21)
+    STM32_REGISTER_END
+};
+
+
+// RCC_APB1RSTR Register
+namespace rcc_apb1rstr{
+    struct bitfield_t{
+        uint32_t  Tim2              :   1;  // 00
+        uint32_t  Tim3              :   1;  // 01
+        uint32_t  Tim4              :   1;  // 02
+        uint32_t  Tim5              :   1;  // 03
+        uint32_t  Tim6              :   1;  // 04
+        uint32_t  Tim7              :   1;  // 05
+        uint32_t  Tim12             :   1;  // 06
+        uint32_t  Tim13             :   1;  // 07
+        uint32_t  Tim14             :   1;  // 08
+        uint32_t  _res_09           :   2;  // 09
+        uint32_t  Wwdg              :   1;  // 11
+        uint32_t  _res_12           :   2;  // 12
+        uint32_t  Spi2              :   1;  // 14
+        uint32_t  Spi3              :   1;  // 15
+        uint32_t  _res_16           :   1;  // 16
+        uint32_t  Usart2            :   1;  // 17
+        uint32_t  Usart3            :   1;  // 18
+        uint32_t  Uart4             :   1;  // 19
+        uint32_t  Uart5             :   1;  // 20
+        uint32_t  I2c1              :   1;  // 21
+        uint32_t  I2c2              :   1;  // 22
+        uint32_t  Usb               :   1;  // 23
+        uint32_t  _res_24           :   1;  // 24
+        uint32_t  Can               :   1;  // 25
+        uint32_t  _res_26           :   1;  // 26
+        uint32_t  Bkp               :   1;  // 27
+        uint32_t  Pwr               :   1;  // 28
+        uint32_t  Dac               :   1;  // 29
+        uint32_t  _res_30           :   2;  // 30
+    };
+    
+    STM32_REGISTER_START(bitfield_t, 0x40021010)
+        STM32_ACCESSOR_BB(Tim2      , 0)
+        STM32_ACCESSOR_BB(Tim3      , 1)
+        STM32_ACCESSOR_BB(Tim4      , 2)
+        STM32_ACCESSOR_BB(Tim5      , 3)
+        STM32_ACCESSOR_BB(Tim6      , 4)
+        STM32_ACCESSOR_BB(Tim7      , 5)
+        STM32_ACCESSOR_BB(Tim12     , 6)
+        STM32_ACCESSOR_BB(Tim13     , 7)
+        STM32_ACCESSOR_BB(Tim14     , 8)
+        STM32_ACCESSOR_BB(Wwdg      , 11)
+        STM32_ACCESSOR_BB(Spi2      , 14)
+        STM32_ACCESSOR_BB(Spi3      , 15)
+        STM32_ACCESSOR_BB(Usart2    , 17)
+        STM32_ACCESSOR_BB(Usart3    , 18)
+        STM32_ACCESSOR_BB(Uart4     , 19)
+        STM32_ACCESSOR_BB(Uart5     , 20)
+        STM32_ACCESSOR_BB(I2c1      , 21)
+        STM32_ACCESSOR_BB(I2c2      , 22)
+        STM32_ACCESSOR_BB(Usb       , 23)
+        STM32_ACCESSOR_BB(Can       , 25)
+        STM32_ACCESSOR_BB(Bkp       , 27)
+        STM32_ACCESSOR_BB(Pwr       , 28)
+        STM32_ACCESSOR_BB(Dac       , 29)
+    STM32_REGISTER_END
+};
+
+
+// RCC_AHBENR Register
+namespace rcc_ahbenr{
+    struct bitfield_t{
+        uint32_t  Dma1              :   1;  // 00
+        uint32_t  Dma2              :   1;  // 01
+        uint32_t  Sram              :   1;  // 02
+        uint32_t  _res_03           :   1;  // 03
+        uint32_t  Flitf             :   1;  // 04
+        uint32_t  _res05            :   1;  // 05
+        uint32_t  Crc               :   1;  // 06
+        uint32_t  _res_07           :   1;  // 07
+        uint32_t  Fsmc              :   1;  // 08
+        uint32_t  _res_09           :   1;  // 09
+        uint32_t  Sdio              :   1;  // 10
+        uint32_t  _res_12           :   21; // 11
+    };
+    
+    STM32_REGISTER_START(bitfield_t, 0x40021014)
+        STM32_ACCESSOR_BB(Dma1      , 0)
+        STM32_ACCESSOR_BB(Dma2      , 1)
+        STM32_ACCESSOR_BB(Sram      , 2)
+        STM32_ACCESSOR_BB(Flitf     , 4)
+        STM32_ACCESSOR_BB(Crc       , 6)
+        STM32_ACCESSOR_BB(Fsmc      , 8)
+        STM32_ACCESSOR_BB(Sdio      , 10)
+    STM32_REGISTER_END
+};
+
+// RCC_APB2ENR Register
+namespace rcc_apb2enr{
+    struct bitfield_t{
+        uint32_t  Afio              :   1;  // 00
+        uint32_t  _res_01           :   1;  // 01
+        uint32_t  Iopa              :   1;  // 02
+        uint32_t  Iopb              :   1;  // 03
+        uint32_t  Iopc              :   1;  // 04
+        uint32_t  Iopd              :   1;  // 05
+        uint32_t  Iope              :   1;  // 06
+        uint32_t  Iopf              :   1;  // 07
+        uint32_t  Iopg              :   1;  // 08
+        uint32_t  Adc1              :   1;  // 09
+        uint32_t  Adc2              :   1;  // 10
+        uint32_t  Tim1              :   1;  // 11
+        uint32_t  Spi1              :   1;  // 12
+        uint32_t  Tim8              :   1;  // 13
+        uint32_t  Usart1            :   1;  // 14
+        uint32_t  Adc3              :   1;  // 15
+        uint32_t  _res_16           :   3;  // 16
+        uint32_t  Tim9              :   1;  // 19
+        uint32_t  Tim10             :   1;  // 20
+        uint32_t  Tim11             :   1;  // 21
+        uint32_t  _res_24           :   10; // 22
+    };
+    
+    STM32_REGISTER_START(bitfield_t, 0x40021018)
+        STM32_ACCESSOR_BB(Afio      , 0)
+        STM32_ACCESSOR_BB(Iopa      , 2)
+        STM32_ACCESSOR_BB(Iopb      , 3)
+        STM32_ACCESSOR_BB(Iopc      , 4)
+        STM32_ACCESSOR_BB(Iopd      , 5)
+        STM32_ACCESSOR_BB(Iope      , 6)
+        STM32_ACCESSOR_BB(Iopf      , 7)
+        STM32_ACCESSOR_BB(Iopg      , 8)
+        STM32_ACCESSOR_BB(Adc1      , 9)
+        STM32_ACCESSOR_BB(Adc2      , 10)
+        STM32_ACCESSOR_BB(Tim1      , 11)
+        STM32_ACCESSOR_BB(Spi1      , 12)
+        STM32_ACCESSOR_BB(Tim8      , 13)
+        STM32_ACCESSOR_BB(Usart1    , 14)
+        STM32_ACCESSOR_BB(Adc3      , 15)
+        STM32_ACCESSOR_BB(Tim9      , 19)
+        STM32_ACCESSOR_BB(Tim10     , 20)
+        STM32_ACCESSOR_BB(Tim11     , 21)
+    STM32_REGISTER_END
+};
+
+
+// RCC_APB1ENR Register
+namespace rcc_apb1enr{
+    struct bitfield_t{
+        uint32_t  Tim2              :   1;  // 00
+        uint32_t  Tim3              :   1;  // 01
+        uint32_t  Tim4              :   1;  // 02
+        uint32_t  Tim5              :   1;  // 03
+        uint32_t  Tim6              :   1;  // 04
+        uint32_t  Tim7              :   1;  // 05
+        uint32_t  Tim12             :   1;  // 06
+        uint32_t  Tim13             :   1;  // 07
+        uint32_t  Tim14             :   1;  // 08
+        uint32_t  _res_09           :   2;  // 09
+        uint32_t  Wwdg              :   1;  // 11
+        uint32_t  _res_12           :   2;  // 12
+        uint32_t  Spi2              :   1;  // 14
+        uint32_t  Spi3              :   1;  // 15
+        uint32_t  _res_16           :   1;  // 16
+        uint32_t  Usart2            :   1;  // 17
+        uint32_t  Usart3            :   1;  // 18
+        uint32_t  Uart4             :   1;  // 19
+        uint32_t  Uart5             :   1;  // 20
+        uint32_t  I2c1              :   1;  // 21
+        uint32_t  I2c2              :   1;  // 22
+        uint32_t  Usb               :   1;  // 23
+        uint32_t  _res_24           :   1;  // 24
+        uint32_t  Can               :   1;  // 25
+        uint32_t  _res_26           :   1;  // 26
+        uint32_t  Bkp               :   1;  // 27
+        uint32_t  Pwr               :   1;  // 28
+        uint32_t  Dac               :   1;  // 29
+        uint32_t  _res_30           :   2;  // 30
+    };
+    
+    STM32_REGISTER_START(bitfield_t, 0x4002101C)
+        STM32_ACCESSOR_BB(Tim2      , 0)
+        STM32_ACCESSOR_BB(Tim3      , 1)
+        STM32_ACCESSOR_BB(Tim4      , 2)
+        STM32_ACCESSOR_BB(Tim5      , 3)
+        STM32_ACCESSOR_BB(Tim6      , 4)
+        STM32_ACCESSOR_BB(Tim7      , 5)
+        STM32_ACCESSOR_BB(Tim12     , 6)
+        STM32_ACCESSOR_BB(Tim13     , 7)
+        STM32_ACCESSOR_BB(Tim14     , 8)
+        STM32_ACCESSOR_BB(Wwdg      , 11)
+        STM32_ACCESSOR_BB(Spi2      , 14)
+        STM32_ACCESSOR_BB(Spi3      , 15)
+        STM32_ACCESSOR_BB(Usart2    , 17)
+        STM32_ACCESSOR_BB(Usart3    , 18)
+        STM32_ACCESSOR_BB(Uart4     , 19)
+        STM32_ACCESSOR_BB(Uart5     , 20)
+        STM32_ACCESSOR_BB(I2c1      , 21)
+        STM32_ACCESSOR_BB(I2c2      , 22)
+        STM32_ACCESSOR_BB(Usb       , 23)
+        STM32_ACCESSOR_BB(Can       , 25)
+        STM32_ACCESSOR_BB(Bkp       , 27)
+        STM32_ACCESSOR_BB(Pwr       , 28)
+        STM32_ACCESSOR_BB(Dac       , 29)
+    STM32_REGISTER_END
+};
+
+// RCC_BDCR Register
+namespace rcc_bdcr{
+    struct bitfield_t{
+        uint32_t  Lson              :   1;  // 00
+        uint32_t  Lserdy            :   1;  // 01
+        uint32_t  Lsebyp            :   1;  // 02
+        uint32_t  _res_03           :   5;  // 03
+        uint32_t  Rtcsel            :   2;  // 08
+        uint32_t  _res_10           :   5;  // 10
+        uint32_t  Rtcen             :   1;  // 15
+        uint32_t  Bdrst             :   1;  // 16
+        uint32_t  _res_17           :  15;  // 17
+    };
+    
+    enum class RTCSEL : uint32_t{
+        NOCLK                       =   0,
+        LSECLK                      =   1,
+        LSICLK                      =   2,
+        HSE_DIV_BY_128              =   3,
+    };
+    
+    STM32_REGISTER_START(bitfield_t, 0x40021020)
+        STM32_ACCESSOR_BB(Lson      , 0)
+        STM32_GETTER_BB  (Lserdy    , 1)
+        STM32_ACCESSOR_BB(Lsebyp    , 2)
+        STM32_ACCESSOR_BF(Rtcsel    , RTCSEL)
+        STM32_ACCESSOR_BB(Rtcen     , 15)
+        STM32_ACCESSOR_BB(Bdrst     , 16)
+    STM32_REGISTER_END
+};
+
+// RCC_CSR Register
+namespace rcc_csr{
+    struct bitfield_t{
+        uint32_t  Lsion             :   1;  // 00
+        uint32_t  Lsirdy            :   1;  // 01
+        uint32_t  _res_02           :  22;  // 02
+        uint32_t  Rmvf              :   1;  // 24
+        uint32_t  _res_25           :   1;  // 25
+        uint32_t  Pinrstf           :   1;  // 26
+        uint32_t  Porrstf           :   1;  // 27
+        uint32_t  Sftrstf           :   1;  // 28
+        uint32_t  Iwdgrstf          :   1;  // 29
+        uint32_t  Wwdgrstf          :   1;  // 30
+        uint32_t  Lpwrrstf          :   1;  // 31
+    };
+    
+    STM32_REGISTER_START(bitfield_t, 0x40021024)
+        STM32_ACCESSOR_BB(Lsion     , 0)
+        STM32_GETTER_BB  (Lsirdy    , 1)
+        STM32_ACCESSOR_BB(Rmvf      , 24)
+        STM32_ACCESSOR_BB(Pinrstf   , 26)
+        STM32_ACCESSOR_BB(Porrstf   , 27)
+        STM32_ACCESSOR_BB(Sftrstf   , 28)
+        STM32_ACCESSOR_BB(Iwdgrstf  , 29)
+        STM32_ACCESSOR_BB(Wwdgrstf  , 30)
+        STM32_ACCESSOR_BB(Lpwrrstf  , 31)
+    STM32_REGISTER_END
+};
 
 }; }; // End stm32::registers namespace
 
--- testapp/main.cpp
+++ testapp/main.cpp
@@ -1,92 +1,37 @@
 #include <stm32xx/registers/rcc_lmhxl.hpp>
-
-
-// #include "../include/hal/rcc.h"
-// #include "../include/hal/gpio.h"
-// 
-// 
-// #define STM32XX_SETTER_BF(field_name,field_type) \
-//         inline static void set##field_name(field_type f){ \
-//             bitfield().field_name = f; \
-//         }
-//         
-// #define STM32XX_GETTER_BF(field_name,field_type) \
-//         inline static field_type get##field_name(){ \
-//             return static_cast<field_type>(bitfield().field_name); \
-//         }
-//         
-// #define STM32XX_ACCESSOR_BF(field_name,field_type) \
-//         STM32XX_GETTER_BF(field_name,field_type) \
-//         STM32XX_SETTER_BF(field_name,field_type)
-// 
-// #define STM32XX_SETTER_BB(field_name, bit) \
-//         inline static void set##field_name(bool f){ \
-//             bitband(bit) = f; \
-//         }
-// 
-// #define STM32XX_GETTER_BB(field_name, bit) \
-//         inline static bool get##field_name(){ \
-//             return bitband(bit);\
-//         }
-//         
-// #define STM32XX_TOGGLER_BB(field_name, bit) \
-//         inline static void toggle##field_name(){ \
-//             bitband(bit) = !bitband(bit); \
-//         }
-//         
-// #define STM32XX_ACCESSOR_BB(field_name, bit) \
-//         STM32XX_GETTER_BB(field_name, bit) \
-//         STM32XX_SETTER_BB(field_name, bit) \
-//         STM32XX_TOGGLER_BB(field_name, bit) 
-//         
-// #define STM32XX_REGISTER_START(bitfield_type, reg_address)                          \
-//         class reg : public ::internals::BasicRegister<bitfield_type, reg_address>{  \
-//         public:                                                                     \
-//             reg(){};
-//         
-// #define STM32XX_REGISTER_END  };
-//             
-// namespace internals{
-//     
-// template <typename BitField, uint32_t Address>
-// class BasicRegister{
-// public:
-//     //static constexpr uint32_t _address = Address;
-//     
-// public:
-//     BasicRegister() { };
-//     inline static constexpr volatile uint32_t& value(){
-//         return *reinterpret_cast<volatile uint32_t*>(Address);
-//     };
-//     inline static constexpr volatile BitField& bitfield(){
-//         return *reinterpret_cast<volatile BitField*>(Address);
-//     };
-//     inline static constexpr volatile bool& bitband(uint32_t bit){
-//         return *reinterpret_cast<volatile bool*>(reinterpret_cast<volatile uint32_t*>(0x42000000+(Address-0x40000000)*32+bit*4));
-//     };
-//     
-// };
-// 
-// };
-
-
-
-
-
-
+#include <stm32xx/registers/gpio.hpp>
 
 
 using namespace stm32::registers;
 
 
+void delay ( void ){
+    volatile int count = 1000 * 100;
+    while ( count-- );
+}
+
+void bigdelay ( void ){
+    volatile int count = 5000 * 100;
+    while ( count-- );
+}
+
 int main(void) {
-    //stm32::registers::rcc_cr::reg::value() = 10;
-    rcc_cfrg::reg::setAdcpre(rcc_cfrg::ADCPRE::PCLK_DIV_BY_6);
-//     //myreg::reg::value() = (myreg::reg::value() & (0xFFFFFF00)) | 2;
-//     //myreg::reg::bitfield().Field0=2;
-//     myreg::reg::setBits0(true);
-//     myreg::reg::setField0(myreg::Field0::CONFIG_D);
-//     temp = myreg::reg::getField0();
-//     myreg::reg::toggleBits0();
-    //return myreg::reg::value();
+    rcc_apb2enr::reg::setIopc(true);
+    gpio::c::crh::reg::setCnf13(gpio::CNF::GP_PUSH_PULL);
+    gpio::c::crh::reg::setMode13(gpio::MODE::OUTPUT_50MHZ);
+    gpio::c::crh::reg::setCnf14(gpio::CNF::GP_PUSH_PULL);
+    gpio::c::crh::reg::setMode14(gpio::MODE::OUTPUT_50MHZ);
+
+    while(true){
+        for (int it_i=0; it_i<5; it_i++){
+            gpio::c::odr::reg::setOdr13(false);
+            delay();
+            gpio::c::odr::reg::setOdr13(true);
+            delay();
+        }
+        gpio::c::odr::reg::setOdr14(false);
+        bigdelay();
+        gpio::c::odr::reg::setOdr14(true);
+    };
+
 };
